2e6bcee512ea2e28f6812ef8a3ed7ecad995007f
nico <nicolenzi1@hotmail.com>
2022-11-30 20:03:06 -0300

    v1.6

--- VentanaPedidos.py
+++ VentanaPedidos.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
+from datetime import date
+from datetime import datetime
+
+fecha = datetime.now()
 
 ventanaPed = tk.Tk()
 ventanaPed.title("Pedidos")
@@ -9,6 +13,8 @@ ventanaPed.geometry("800x600")
 ventanaPed.configure(bg="#AED6F1")
 
 #frames
+
+
 frameOpc = Frame(ventanaPed,
            width="90",
            height="600",
@@ -18,14 +24,26 @@ frameOpc.pack(side=LEFT)
 frameCentro = Frame(ventanaPed,
            width="710",
            height="600",
-           bg="snow")
+           bg="red")
 frameCentro.pack(side=LEFT)
 
 #Labels
 txtFecha = Label(frameCentro,
-            text="fecha",
-            bg="snow")
+           text="fecha",
+           bg="snow")
 txtFecha.grid(row=0,column=0)
+
+txtFechaAct = Label(frameCentro,
+              text=fecha)
+txtFechaAct.grid(row=0,column=1)
+
+txtNom = Label(frameCentro,
+         text="Usuario")
+txtNom.grid(row=0,column=2)
+
+
+
+
 
 
 ventanaPed.mainloop()
