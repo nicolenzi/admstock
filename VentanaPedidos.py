503877fd3e7136f1d7326819d211bed80b800bb4
nico <nicolenzi1@hotmail.com>
2022-12-01 20:32:27 -0300

    v1.7

--- VentanaPedidos.py
+++ VentanaPedidos.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 from datetime import date
@@ -24,25 +25,58 @@ frameOpc.pack(side=LEFT)
 frameCentro = Frame(ventanaPed,
            width="710",
            height="600",
-           bg="red")
+           bg="orange")
 frameCentro.pack(side=LEFT)
 
 #Labels
 txtFecha = Label(frameCentro,
-           text="fecha",
+           text="fecha: ",
            bg="snow")
-txtFecha.grid(row=0,column=0)
+txtFecha.place(relx=0.1,rely=0.3)
 
 txtFechaAct = Label(frameCentro,
               text=fecha)
-txtFechaAct.grid(row=0,column=1)
+txtFechaAct.place(relx=0.2,rely=0.3)
 
-txtNom = Label(frameCentro,
-         text="Usuario")
-txtNom.grid(row=0,column=2)
+txtNomUsr = Label(frameCentro,
+         text="Usuario: ")
+txtNomUsr.place(relx=0.6,rely=0.3)
 
+txtNomUsrAct = Label(frameCentro,
+               text="usr",
+               bg="white")
+txtNomUsrAct.place(relx=0.7,rely=0.3)
 
+txtProd = Label(frameCentro,
+          text="Producto:")
+txtProd.place(relx=0.1,rely=0.4)
 
+txtCant = Label(frameCentro,
+          text="Cantidad:")
+txtCant.place(relx=0.5,rely=0.4)
+
+txtPedRec = Label(frameCentro,
+            text="Pedidos realizados recientemente:")
+txtPedRec.place(relx=0.1,rely=0.6)
+
+#Combobox
+ListaProd = ttk.Combobox(frameCentro,
+            state="readonly",
+            values=["Medialunas","Tortas pinchadas","Tortas raspadas"])
+ListaProd.place(relx=0.2,rely=0.4)
+
+#Entrys
+entryCant = Entry(frameCentro,
+            width="8")
+entryCant.place(relx=0.6,rely=0.4)
+
+#Botones 
+btnAñadir = Button(frameCentro, text="+")
+btnAñadir.place(relx=0.89,rely=0.4)
+
+btnCalc = Button(frameCentro,
+          text="Calculadora")
+btnCalc.place(relx=0.71,rely=0.4)
 
 
 
