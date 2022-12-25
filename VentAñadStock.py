d34185adce15fa608972aaf7b2a32bed157975ee
nico <nicolenzi1@hotmail.com>
2022-12-25 12:25:09 -0300

    V2.4

--- VentAñadStock.py
+++ VentAñadStock.py
@@ -28,7 +28,7 @@ curs = sqlconex.cursor()
 
 #           FRAMES
 #   Frame Izquierdo
-frameIzq = Frame(ventAñadir, bg="#85C1E9",width="100",height="580")
+frameIzq = Frame(ventAñadir, bg="#85C1E9",width="160",height="580")
 frameIzq.place(relx=0,rely=0)
 
 #Labels
@@ -48,33 +48,45 @@ btnVolver.grid(row=1,column=0,pady=250)
 frameCent = Frame(ventAñadir, bg="white", width="740",height="580")
 frameCent.place(relx=0.099,rely=0)
 
+frameBg = Frame(frameCent, bg="#F3F3F3",width="100",height="100")
+frameBg.place(relx=0.12,rely=0.2)
+
 #Labels
 txtAñadStk = Label(ventAñadir, text="Añadir stock",bg="#F3F3F3", font=("source",14))
-txtAñadStk.place(relx=0.2,rely=0.1)
+txtAñadStk.place(relx=0.15,rely=0.13)
 
 txtMercad = Label(ventAñadir, text="Mercaderia",bg="white", font=("courier",11))
-txtMercad.place(relx=0.2,rely=0.2)
+txtMercad.place(relx=0.15,rely=0.25)
 
 txtCant = Label(ventAñadir, text="Cantidad", bg="white", font=("courier",11))
-txtCant.place(relx=0.45,rely=0.2)
+txtCant.place(relx=0.49,rely=0.25)
 
 txtFecha = Label(ventAñadir, text="Fecha", bg="white", font=("courier",11))
-txtFecha.place(relx=0.66,rely=0.2)
+txtFecha.place(relx=0.15,rely=0.34)
+
+txtUsr = Label(ventAñadir, text="Usuario", bg="white", font=("courier",11))
+txtUsr.place(relx=0.49,rely=0.34)
+
+txtUsuario = Label(ventAñadir, text="Actual",bg="white", font=("courier",11))
+txtUsuario.place(relx=0.6,rely=0.34)
 
 #Botones
 
+btnCalc = Button(ventAñadir, text="Calculadora",bd=0,width="7")
+btnCalc.place(relx=0.71,rely=0.24)
+
 #Entrys
 entryCant = Entry(ventAñadir,width="10")
-entryCant.place(relx=0.54, rely=0.2)
+entryCant.place(relx=0.6, rely=0.25)
 
 #Comboboxs
 comboMercad = ttk.Combobox(ventAñadir,state="readonly",width="10",values=["Medialunas","pinchadas","raspadas"])
-comboMercad.place(relx=0.31,rely=0.2)
+comboMercad.place(relx=0.28,rely=0.25)
 
 
 comboFecha = ttk.Combobox(ventAñadir,state="readonly",width="10",values=
 ["Actual(hoy)","Otra"])
-comboFecha.place(relx=0.72,rely=0.2)
+comboFecha.place(relx=0.28,rely=0.34)
 
 #_
 
